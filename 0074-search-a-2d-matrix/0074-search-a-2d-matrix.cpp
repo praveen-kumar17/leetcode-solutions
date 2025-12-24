94196bcdbe990d8e4636f247192d1f9e7d5292dc
praveen-kumar17 <praveenbarre9@gmail.com>
2025-12-24 18:36:42 +0530

    Time: 0 ms (100%), Space: 13.3 MB (74.92%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    bool possi(vector<int>& mat, int target){
-        int n=mat.size();
-        int low=0,high=n-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(mat[mid]==target){
-                return true;
-            }
-            else if(mat[mid]>target){
-                high=mid-1;
-            }else{
-                low=mid+1;
-            }
-        }
-        return false;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
-        int low=0;
-        int high=n-1;
-        for(int i=0;i<n;i++){
-            if(matrix[i][0]<=target && target<=matrix[i][m-1]){
-                return possi(matrix[i],target);
+        int low=0,high=(m*n)-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int r=mid/m;
+            int c=mid%m;
+            if(matrix[r][c]==target){
+                return true;
+            } else if(matrix[r][c]<target){
+                low=mid+1;
+            } else{
+                high=mid-1;
             }
         }
         return false;
