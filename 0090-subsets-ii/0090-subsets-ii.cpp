44989871ecac8e2280544eec0d7d4069ef6ce946
praveen-kumar17 <praveenbarre9@gmail.com>
2026-01-21 19:13:45 +0530

    Time: 0 ms (100%), Space: 10.3 MB (84.39%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,20 @@
 class Solution {
+private:
+    void combination(vector<int>& nums,vector<vector<int>>& res,vector<int>& ans,int idx){
+        res.push_back(ans);
+        for(int i=idx;i<nums.size();i++){
+            if(i>idx && nums[i]==nums[i-1]) continue;
+            ans.push_back(nums[i]);
+            combination(nums,res,ans,i+1);
+            ans.pop_back();
+        }
+    }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n=nums.size();
         sort(nums.begin(),nums.end());
-        set<vector<int>> mpp;
         vector<vector<int>> res;
-        for(int i=0;i<(1<<n);i++){
-            vector<int> ans;
-            for(int j=0;j<n;j++){
-                if(i&(1<<j)){
-                    ans.push_back(nums[j]);
-                }
-            }
-            mpp.insert(ans);
-        }
-        for(auto& it: mpp){
-            res.push_back(it);
-        }
+        vector<int> ans;
+        combination(nums,res,ans,0);
         return res;
     }
 };
