2e992ac776399e06d3e4b806d11f9b63ed376bfd
praveen-kumar17 <praveenbarre9@gmail.com>
2026-04-10 10:15:07 +0530

    Time: 8 ms (94.43%), Space: 47.3 MB (21.38%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,121 +1,46 @@
 class Solution {
 public:
-        vector<int> findNSE(vector<int> &arr) {
-        
-        // Size of array
-        int n = arr.size();
-        
-        // To store the answer
+    vector<int> findPSE(vector<int> & arr){
+        int n=arr.size();
         vector<int> ans(n);
-        
-        // Stack 
         stack<int> st;
-        
-        // Start traversing from the back
-        for(int i = n - 1; i >= 0; i--) {
-            
-            // Get the current element
-            int currEle = arr[i];
-            
-            /* Pop the elements in the stack until 
-            the stack is not empty and the top 
-            element is not the smaller element */
-            while(!st.empty() && arr[st.top()] >= arr[i]){
+        for(int i=0;i<n;i++){
+            int el=arr[i];
+            while(!st.empty() && arr[st.top()]>arr[i]){
                 st.pop();
             }
-            
-            // Update the answer
-            ans[i] = !st.empty() ? st.top() : n;
-            
-            /* Push the index of current 
-            element in the stack */
+            ans[i]=!st.empty() ? st.top():-1;
             st.push(i);
         }
-        
-        // Return the answer
         return ans;
     }
-    
-    /* Function to find the indices of 
-    previous smaller or equal elements */
-    vector<int> findPSEE(vector<int> &arr) {
-        
-        // Size of array
-        int n = arr.size();
-        
-        // To store the answer
+    vector<int>findNSE(vector<int> & arr){
+        int n=arr.size();
         vector<int> ans(n);
-        
-        // Stack 
         stack<int> st;
-        
-        // Traverse on the array
-        for(int i=0; i < n; i++) {
-            
-            // Get the current element
-            int currEle = arr[i];
-            
-            /* Pop the elements in the stack until 
-            the stack is not empty and the top 
-            elements are greater than the current element */
-            while(!st.empty() && arr[st.top()] > arr[i]){
+        for(int i=n-1;i>=0;i--){
+            int el=arr[i];
+            while(!st.empty() && arr[st.top()]>=arr[i]){
                 st.pop();
             }
-            
-            // Update the answer
-            ans[i] = !st.empty() ? st.top() : -1;
-            
-            /* Push the index of current 
-            element in the stack */
+            ans[i]=!st.empty() ? st.top():n;
             st.push(i);
         }
-        
-        // Return the answer
         return ans;
     }
-    
-public:
-
-    /* Function to find the sum of the 
-    minimum value in each subarray */
-    int sumSubarrayMins(vector<int> &arr) {
-        
-        vector<int> nse = 
-            findNSE(arr);
-        
-        vector<int> psee =
-            findPSEE(arr);
-        
-        // Size of array
-        int n = arr.size();
-        
-        int mod = 1e9 + 7; // Mod value
-        
-        // To store the sum
-        int sum = 0;
-        
-        // Traverse on the array
-        for(int i=0; i < n; i++) {
-            
-            // Count of first type of subarrays
-            int left = i - psee[i];
-            
-            // Count of second type of subarrays
-            int right = nse[i] - i;
-            
-            /* Count of subarrays where 
-            current element is minimum */
-            long long freq = left*right*1LL;
-            
-            // Contribution due to current element 
-            int val = (freq*arr[i]*1LL) % mod;
-            
-            // Updating the sum
-            sum = (sum + val) % mod;
+    int sumSubarrayMins(vector<int>& arr) {
+        vector<int> NSE=findNSE(arr);
+        vector<int> PSE=findPSE(arr);
+        int n=arr.size();
+        int sum=0;
+        int mod=1e9+7;
+        for(int i=0;i<n;i++){
+            int left=i-PSE[i];
+            int right=NSE[i]-i;
+            long long freq=(left*right*1LL);
+            long long res=(freq*arr[i]*1LL)%mod;
+            sum=(sum+res)%mod;
         }
-        
-        // Return the computed sum
         return sum;
     }
-
 };
