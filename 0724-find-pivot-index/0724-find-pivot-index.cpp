cb5c79da7bc1299738ba5641fa0e2a02f2664cae
praveen-kumar17 <praveenbarre9@gmail.com>
2026-05-26 11:12:54 +0530

    Time: 0 ms (100%), Space: 35.8 MB (27.84%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -4,8 +4,8 @@ public:
         long long total=accumulate(nums.begin(),nums.end(),0);
         long long prefix_sum=0;
         for(int i=0;i<nums.size();i++){
-            long long right=total-prefix_sum-nums[i];
-            if(right==prefix_sum){
+            long long suffix_sum=total-prefix_sum-nums[i];
+            if(prefix_sum==suffix_sum){
                 return i;
             }
             prefix_sum+=nums[i];
