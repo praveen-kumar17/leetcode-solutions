ed0446de8599726ecfbd192126ac50e001f39877
praveen-kumar17 <praveenbarre9@gmail.com>
2026-05-28 13:44:46 +0530

    Time: 836 ms (19.42%), Space: 11.7 MB (29.41%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    bool isit(vector<vector<char>>& board,string word,int i,int j,int idx){
+    bool isit(vector<vector<char>>&board,string word,int idx,int i,int j){
         if(idx==word.size()) return true;
         if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]!=word[idx]) return false;
         char temp=board[i][j];
         board[i][j]='#';
-        bool found=(isit(board,word,i+1,j,idx+1) || isit(board,word,i-1,j,idx+1) || isit(board,word,i,j+1,idx+1) || isit(board,word,i,j-1,idx+1));
+        bool found=(isit(board,word,idx+1,i+1,j) || isit(board,word,idx+1,i-1,j) || isit(board,word,idx+1,i,j+1) || isit(board,word,idx+1,i,j-1));
         board[i][j]=temp;
         return found;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int row=board.size();
-        int coll=board[0].size();
-        for(int i=0;i<row;i++){
-            for(int j=0;j<coll;j++){
-                if(isit(board,word,i,j,0)){
+        int n=board.size();
+        int m=board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(isit(board,word,0,i,j)){
                     return true;
                 }
             }
