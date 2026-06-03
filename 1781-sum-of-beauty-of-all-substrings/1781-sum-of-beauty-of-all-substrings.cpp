cc74d94df63ddbbe9aa3d8716f687a2f8419bbb8
praveen-kumar17 <praveenbarre9@gmail.com>
2026-06-03 14:12:42 +0530

    Time: 127 ms (22.52%), Space: 25.9 MB (36.46%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -4,17 +4,17 @@ public:
         int n=s.length();
         int sum=0;
         for(int i=0;i<n;i++){
-            unordered_map<char,int> mpp;
+            unordered_map<int,int> mpp;
             for(int j=i;j<n;j++){
                 int mini=INT_MAX;
                 int maxi=INT_MIN;
                 mpp[s[j]]++;
                 for(auto it:mpp){
-                mini=min(mini,it.second);
-                maxi=max(maxi,it.second); 
+                    mini=min(mini,it.second);
+                    maxi=max(maxi,it.second);
                 }
-            sum+=(maxi-mini);
-            }   
+                sum+=(maxi-mini);
+            }
         }
         return sum;
     }
