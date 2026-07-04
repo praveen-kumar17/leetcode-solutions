607c4f973b6678b85eb706dc85d097112bd2293a
praveen-kumar17 <praveenbarre9@gmail.com>
2026-07-04 05:54:27 +0530

    Time: 0 ms (100%), Space: 12.8 MB (77.86%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -18,6 +18,6 @@ public:
         if(p==nullptr || q==nullptr){
             return false;
         }
-        return ((p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
+        return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
