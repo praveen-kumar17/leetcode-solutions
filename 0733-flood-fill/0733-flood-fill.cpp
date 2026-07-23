0c5eaa8460c2679276baaa1dd78da1661a8032a8
BARRE PRAVEEN KUMAR <praveenbarre9@gmail.com>
2026-07-23 13:03:25 +0530

    Time: 0 ms (100%), Space: 18.2 MB (57.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    void dfs(int sr,int sc,vector<vector<int>>& image,int color,int intial_col,int drow[],int dcol[]){
-        image[sr][sc]=color;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n=image.size();
         int m=image[0].size();
-        for(int i=0;i<4;i++){
-            int row=sr+drow[i];
-            int col=sc+dcol[i];
-            if(row>=0 && col>=0 && row<n && col<m && image[row][col]==intial_col){
-                dfs(row,col,image,color,intial_col,drow,dcol);
-            }
-        }
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int intial_col=image[sr][sc];
-        if(intial_col==color){
-            return image;
-        }
+        if(intial_col==color) return image;
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        image[sr][sc]=color;
         int drow[]={-1,0,1,0};
         int dcol[]={0,1,0,-1};
-        dfs(sr,sc,image,color,intial_col,drow,dcol);
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int n_row=row+drow[i];
+                int n_col=col+dcol[i];
+                if(n_row>=0 && n_col>=0 && n_row<n && n_col<m && image[n_row][n_col]==intial_col){
+                    image[n_row][n_col]=color;
+                    q.push({n_row,n_col});
+                }
+            }
+        }
         return image;
     }
 };
