fb8068f0109301a4f9fdf017a30dd06c30df69a3
BARRE PRAVEEN KUMAR <praveenbarre9@gmail.com>
2026-07-25 09:33:34 +0530

    Time: 55 ms (65.43%), Space: 21.3 MB (37.89%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string,int>>q;
+        q.push({beginWord,1});
+        unordered_set<string> st(wordList.begin(),wordList.end());
+        st.erase(beginWord);
+        while(!q.empty()){
+            string word=q.front().first;
+            int seq=q.front().second;
+            q.pop();
+            if(word==endWord) return seq;
+            for(int i=0;i<word.size();i++){
+                char orginal=word[i];
+                for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
+                    if(st.find(word)!=st.end()){
+                        st.erase(word);
+                        q.push({word,seq+1});
+                    }
+                }
+                word[i]=orginal;
+            }
+        }
+        return 0;
+    }
+};
