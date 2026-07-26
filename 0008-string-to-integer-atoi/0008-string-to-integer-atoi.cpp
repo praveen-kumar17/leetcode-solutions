ed1950a37d6804324a5fdf6c9071bc333de03abb
BARRE PRAVEEN KUMAR <praveenbarre9@gmail.com>
2026-07-26 18:58:47 +0530

    Time: 0 ms (100%), Space: 10.3 MB (5.28%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    long long recursivecalls(string &s,int i,long long ans,int sign){
-        if(i>=s.length() || !isdigit(s[i])){
+    long long generate(string s,int i,int sign,long long ans){
+        int n=s.length();
+        if(i>=n || !isdigit(s[i])){
             return ans*sign;
         }
         int digit=s[i]-'0';
         if(ans > (INT_MAX-digit)/10){
             return sign==1 ? INT_MAX:INT_MIN;
         }
-        return recursivecalls(s,i+1,ans*10+digit,sign);
+        return generate(s,i+1,sign,(ans*10)+digit);
     }
     int myAtoi(string s) {
         int n=s.length();
@@ -20,11 +21,11 @@ public:
         if(s[i]=='-'){
             sign=-1;
             i++;
-        } else if(s[i]=='+'){
+        }else if(s[i]=='+'){
             sign=1;
             i++;
         }
-        long long res=recursivecalls(s,i,0,sign);
-        return (int)(res);
+        long long ans=generate(s,i,sign,0);
+        return (int)ans;
     }
 };
