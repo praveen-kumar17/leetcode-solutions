a30c59eaa5d941ef051d263889680af19fa563b5
BARRE PRAVEEN KUMAR <praveenbarre9@gmail.com>
2026-08-16 04:21:47 +0530

    Time: 151 ms (36.64%), Space: 17.5 MB (60.6%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -31,7 +31,23 @@ public:
             return false;
         }
         int target=total_sum/2;
-        vector<vector<int>> dp(n,vector<int>(target+1,-1));
-        return f(n-1,target,nums,dp);
+        vector<vector<bool>> dp(n,vector<bool>(target+1,false));
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
+        }
+        if(nums[0]<=target){
+            dp[0][nums[0]]=true;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=target;j++){
+                bool notTake=dp[i-1][j];
+                bool take=false;
+                if(j>=nums[i]){
+                    take=dp[i-1][j-nums[i]];
+                }
+                dp[i][j]=notTake | take;
+            }
+        }
+        return dp[n-1][target];
     }
 };
