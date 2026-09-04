7e8b39d3b564dcab39acabf35d0886006ec24980
BARRE PRAVEEN KUMAR <praveenbarre9@gmail.com>
2026-09-05 05:24:15 +0530

    Time: 403 ms (43.41%), Space: 211.5 MB (39.92%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -17,7 +17,19 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return f(prices,0,0,2,dp);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        for(int ind =n-1;ind >=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                for(int trans=1;trans<=2;trans++){
+                    if(buy==0){
+                        dp[ind][buy][trans]=max(0+dp[ind+1][0][trans],-prices[ind]+dp[ind+1][1][trans]);
+                    }
+                    if(buy==1){
+                        dp[ind][buy][trans]=max(0+dp[ind+1][1][trans],prices[ind]+dp[ind+1][0][trans-1]);
+                    }
+                }
+            }
+        }
+        return dp[0][0][2];
     }
 };
