d01ef51c140a9b25641ed0a9ef42591916ecd3ff
BARRE PRAVEEN KUMAR <praveenbarre9@gmail.com>
2026-09-08 10:39:49 +0530

    Time: 42 ms (62.17%), Space: 84.8 MB (83.55%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int,int> freq;
-        int l=0,max_len=0;
-        for(int r=0;r<fruits.size();r++){
-            freq[fruits[r]]++;
-            if(freq.size()>2){
-                freq[fruits[l]]--;
-                if(freq[fruits[l]]==0) freq.erase(fruits[l]);
-                l++;
+        int n=fruits.size();
+        unordered_map<int,int> mpp;
+        int max_len=0;
+        int i=0;
+        for(int j=0;j<n;j++){
+            mpp[fruits[j]]++;
+            while(mpp.size()>2){
+                mpp[fruits[i]]--;
+                if(mpp[fruits[i]]==0){
+                    mpp.erase(fruits[i]);
+                }
+                i++;
             }
-            if(freq.size()<=2){
-                max_len=max(max_len,r-l+1);
-            }
+            max_len=max(max_len,j-i+1);
         }
         return max_len;
     }
