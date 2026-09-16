8ceca2073ac9e08d8fbb789d83032e0c5f5e2e55
BARRE PRAVEEN KUMAR <praveenbarre9@gmail.com>
2026-09-16 09:57:26 +0530

    Time: 0 ms (100%), Space: 71.6 MB (98.42%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int n=nums.size();
-        int max_sum=INT_MIN;
+        int maxi=INT_MIN;
         int sum=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            if(sum>max_sum){
-                max_sum=sum;
+            if(sum>maxi){
+                maxi=sum;
             }
             if(sum<0){
                 sum=0;
             }
         }
-        return max_sum;
+        return maxi;
     }
 };
