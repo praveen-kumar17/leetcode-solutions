0f129208b974bf5468ddbc63f7ca2e4eb54f500e
BARRE PRAVEEN KUMAR <praveenbarre9@gmail.com>
2026-09-16 09:34:25 +0530

    Time: 0 ms (100%), Space: 21.1 MB (30.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,18 +3,21 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        bool firstrow=false;
-        bool firstcol=false;
+        bool first_row=false;
+        bool first_col=false;
+        //row
         for(int i=0;i<m;i++){
             if(matrix[0][i]==0){
-                firstrow=true;
+                first_row=true;
             }
         }
+        // col
         for(int i=0;i<n;i++){
             if(matrix[i][0]==0){
-                firstcol=true;
+                first_col=true;
             }
         }
+        //if any zero occurs make its first rowth and columth ele to zero
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
                 if(matrix[i][j]==0){
@@ -23,25 +26,26 @@ public:
                 }
             }
         }
+        //if any element zero in first row||col make that ele zero
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
                     matrix[i][j]=0;
                 }
             }
         }
-        if(firstrow){
+        //first row zeroes if 
+        if(first_row){
             for(int i=0;i<m;i++){
                 matrix[0][i]=0;
             }
         }
-        if(firstcol){
+        //first col zeroes if 
+        if(first_col){
             for(int i=0;i<n;i++){
                 matrix[i][0]=0;
             }
         }
         return;
-        
-
     }
 };
