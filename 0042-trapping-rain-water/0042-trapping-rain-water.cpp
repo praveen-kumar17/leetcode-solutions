724ba205654922e39a3cd04d82b3781536eaee99
BARRE PRAVEEN KUMAR <praveenbarre9@gmail.com>
2026-09-18 05:08:10 +0530

    Time: 3 ms (15.53%), Space: 26.1 MB (55.73%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -3,17 +3,17 @@ public:
     int trap(vector<int>& height) {
         int n=height.size();
         int l=0,r=n-1;
-        int lmax=height[l],rmax=height[r];
+        int lmax=height[0],rmax=height[n-1];
         int ans=0;
-        while(l<r){
+        while(l<=r){
             if(lmax<rmax){
-                l++;
                 lmax=max(lmax,height[l]);
                 ans+=lmax-height[l];
+                l++;
             }else{
-                r--;
                 rmax=max(rmax,height[r]);
                 ans+=rmax-height[r];
+                r--;
             }
         }
         return ans;
