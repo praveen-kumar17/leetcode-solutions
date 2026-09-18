cd57a7f5518f92f4817554bb680ab07366c65c4d
BARRE PRAVEEN KUMAR <praveenbarre9@gmail.com>
2026-09-18 06:12:42 +0530

    Time: 20 ms (69.36%), Space: 139.2 MB (49.89%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         deque<int> dq;
-        vector<int> res;
+        vector<int> ans;
         for(int i=0;i<nums.size();i++){
             while(!dq.empty() && dq.front()<=i-k){
                 dq.pop_front();
@@ -12,9 +12,9 @@ public:
             }
             dq.push_back(i);
             if(i>=k-1){
-                res.push_back(nums[dq.front()]);
+                ans.push_back(nums[dq.front()]);
             }
         }
-        return res;
+        return ans;
     }
 };
